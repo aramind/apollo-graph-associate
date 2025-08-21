8388ee0fd89e3a055d8621d9e170a9fb276a62d9
aramind <88814622+aramind@users.noreply.github.com>
2025-08-21 14:50:16 +0800

    feat: setting up resolvers

---
+++ server/src/resolvers.js
@@ -0,0 +1,17 @@
+const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate
+    // the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.tracksAPI.getTracksForHome();
+    },
+  },
+
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.tracksAPI.getAuthor(authorId);
+    },
+  },
+};
+
+module.exports = resolvers;
