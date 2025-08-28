38d2a2dc8fc50f1c2367c7b5b45b92fe7cffa586
aramind <88814622+aramind@users.noreply.github.com>
2025-08-28 15:42:43 +0800

    fix: fixing the id undefined via refactoring resolver in the be

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,12 +1,45 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Layout } from "../components";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
 
+import { gql, useQuery } from "@apollo/client";
+
+const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
 const Track = () => {
   const { trackId } = useParams();
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+
+  console.log(data?.track?.id);
   return (
     <Layout>
-      <p>{trackId}</p>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
     </Layout>
   );
 };
