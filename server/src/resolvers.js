b9a959546263044517e7ed404710c184475dffed
aramind <88814622+aramind@users.noreply.github.com>
2025-08-28 17:25:27 +0800

    feat: adding the resolver to handle mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -10,6 +10,28 @@ const resolvers = {
     },
   },
 
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.tracksAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.tracksAPI.getAuthor(authorId);
